91d76702fb5ffacb2308e383b338b60af17d39d2
Giuseppe-lore <loregiuseppe0@gmail.com>
2022-07-08 14:32:56 +0200

    header complete

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,33 @@
 <template>
   <div id="app">
     
+    <PageHeader />
+
+    <!-- Main -->
+    <main>
+      
+    </main>
   </div>
 </template>
 
 <script>
+import PageHeader from "./components/PageHeader.vue";
+
 export default {
   name: 'App',
   components: {
+    PageHeader,
     
   }
 }
 </script>
 
 <style lang="scss">
+@import './style/common';
+@import './style/variables';
+
+main {
+  background-color: $main_color;
+}
 
 </style>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,35 @@
+<template>
+  <header>
+    
+    <!-- Logo -->
+    <div class="img-wrapper">
+      <img src="../assets/img/logo.png" alt="spotify-logo">
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+    name: "PageHeader"
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/common';
+@import '../style/variables';
+
+header {
+  height: 70px;
+  background-color: $secondary_color;
+
+  .img-wrapper {
+    padding: 15px;
+
+    img {
+        width: 40px;
+        }
+  }
+}
+
+</style>
